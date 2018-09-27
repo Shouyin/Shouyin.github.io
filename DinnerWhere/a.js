9be4e6b9f145f32aa6a6d63fa4ed15b60a52f92c
Shouyin <7000kl@163.com>
2018-09-27 21:21:48 +1000

    add options

--- DinnerWhere/a.js
+++ DinnerWhere/a.js
@@ -4,6 +4,17 @@ var ph = document.getElementById("ph");
 var httpRequest = new XMLHttpRequest();
 var rstntString = "";
 
+var renderPage = (rstntString) => {
+    let rstnts = JSON.parse(rstntString);
+    for (let blurAddrs in rstnts){
+        let opt = document.createElement("option");
+        opt.innerHTML = blurAddrs;
+        opt.value = blurAddrs;
+        blurAddrSelect.appendChild(opt);
+    }
+    
+}
+
 httpRequest.onreadystatechange = () => {
     rstntString = httpRequest.responseText;
     ph.innerHTML = rstntString;
